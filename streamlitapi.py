ee7340fbce594cf3205dc877d5b2bd8056b61b71
zeerau <zeerabaidu@gmail.com>
2024-11-24 15:02:11 +0100

    tweaking around

--- streamlitapi.py
+++ streamlitapi.py
@@ -13,7 +13,7 @@ def main():
 
     if st.button('Predict'):
         makeprediction = model.predict([[N,P,K,ph]])
-        output = makeprediction[0]
+        output = makeprediction
         st.success('You can grow {} in your field'.format(output))
 if __name__ == '__main__':
         main()
